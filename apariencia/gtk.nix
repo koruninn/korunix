de14b5bba872696d2ffa9a7db3793d80cad46a46
koruninn <koruninn@gmail.com>
2026-09-12 08:43:27 -0500

    Aislar GTK de la configuración global de Plasma

--- apariencia/gtk.nix
+++ apariencia/gtk.nix
@@ -4,7 +4,9 @@
     pkgs.hatter-icon-theme
   ];
 
-  # Mantiene GTK independiente de cualquier estilo Qt.
+  # GTK mantiene su apariencia independiente de Plasma y Qt.
+  # No se instala un kdeglobals global: Plasma debe conservar sus preferencias
+  # dentro de su propia sesión y no exportarlas a Niri/GTK.
   environment.etc = {
     "gtk-2.0/gtkrc".text = ''
       gtk-icon-theme-name="Hatter-Slate"
@@ -18,13 +20,6 @@
     "gtk-4.0/settings.ini".text = ''
       [Settings]
       gtk-icon-theme-name=Hatter-Slate
-    '';
-
-    # Qt puede consultar este archivo para el nombre del tema de iconos sin
-    # imponer un estilo Qt sobre GTK.
-    "xdg/kdeglobals".text = ''
-      [Icons]
-      Theme=Hatter-Slate
     '';
   };
 }
