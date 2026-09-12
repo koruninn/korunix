39c9bb544c0a2424167723a64739a4eff0a819b8
koruninn <koruninn@gmail.com>
2026-09-12 08:24:26 -0500

    Cambiar Hatter-Green por Hatter-Slate

--- apariencia/gtk.nix
+++ apariencia/gtk.nix
@@ -7,24 +7,24 @@
   # Mantiene GTK independiente de cualquier estilo Qt.
   environment.etc = {
     "gtk-2.0/gtkrc".text = ''
-      gtk-icon-theme-name="Hatter-Green"
+      gtk-icon-theme-name="Hatter-Slate"
     '';
 
     "gtk-3.0/settings.ini".text = ''
       [Settings]
-      gtk-icon-theme-name=Hatter-Green
+      gtk-icon-theme-name=Hatter-Slate
     '';
 
     "gtk-4.0/settings.ini".text = ''
       [Settings]
-      gtk-icon-theme-name=Hatter-Green
+      gtk-icon-theme-name=Hatter-Slate
     '';
 
     # Qt puede consultar este archivo para el nombre del tema de iconos sin
     # imponer un estilo Qt sobre GTK.
     "xdg/kdeglobals".text = ''
       [Icons]
-      Theme=Hatter-Green
+      Theme=Hatter-Slate
     '';
   };
 }
