e73c898848bbf307feb7665b7f47414378a97b28
koruninn <koruninn@gmail.com>
2026-09-12 00:15:03 -0500

    fijar Hatter como tema de iconos sin interferencia de estilo Qt

--- apariencia/gtk.nix
+++ apariencia/gtk.nix
@@ -1,18 +1,30 @@
 { pkgs, ... }: {
-  # 1. Asegura que el paquete de iconos esté instalado en el sistema
+  # Tema de iconos global: Hatter para GTK y aplicaciones que usan la especificación XDG.
   environment.systemPackages = [
     pkgs.hatter-icon-theme
   ];
 
-  # 2. Inyecta el tema de iconos por defecto en las rutas globales de configuración XDG
+  # Mantiene GTK independiente de cualquier estilo Qt.
   environment.etc = {
+    "gtk-2.0/gtkrc".text = ''
+      gtk-icon-theme-name="Hatter-Green"
+    '';
+
     "gtk-3.0/settings.ini".text = ''
       [Settings]
       gtk-icon-theme-name=Hatter-Green
     '';
+
     "gtk-4.0/settings.ini".text = ''
       [Settings]
       gtk-icon-theme-name=Hatter-Green
+    '';
+
+    # Qt puede consultar este archivo para el nombre del tema de iconos sin
+    # imponer un estilo Qt sobre GTK.
+    "xdg/kdeglobals".text = ''
+      [Icons]
+      Theme=Hatter-Green
     '';
   };
 }
