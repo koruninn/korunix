273211617ec16bb6c286ace5af28e99ed3526cfa
koruninn <koruninn@gmail.com>
2026-09-12 21:29:51 -0500

    Usar fetch animado al abrir la terminal

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 # Desactivar el saludo inicial por defecto de Fish
 set -g fish_greeting ""
 
-# Ejecutar fastfetch solo si la sesión es interactiva (la terminal está abierta)
+# Ejecutar fetch solo si la sesión es interactiva (la terminal está abierta)
 if status is-interactive
-    fastfetch
+    fetch
 end
