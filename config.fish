b90b46522e1da81d966f419c5c945b03612ee8d3
koruninn <koruninn@gmail.com>
2026-09-12 21:33:16 -0500

    Forzar logo de NixOS en fetch

--- config.fish
+++ config.fish
@@ -1,7 +1,8 @@
 # Desactivar el saludo inicial por defecto de Fish
 set -g fish_greeting ""
 
-# Ejecutar fetch solo si la sesión es interactiva (la terminal está abierta)
+# Ejecutar fetch solo si la sesión es interactiva (la terminal está abierta).
+# Forzamos el mismo logo pequeño de NixOS que usaba Fastfetch.
 if status is-interactive
-    fetch
+    fetch -l nixos_small
 end
