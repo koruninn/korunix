f4ee6815cff95874e352bf611a02e1c4298cc7db
koruninn <koruninn@gmail.com>
2026-09-13 01:25:03 -0500

    Centralizar funciones modernas de Nix

--- modulos/base/herramientas.nix
+++ modulos/base/herramientas.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{pkgs, ...}: {
+  # Todos los equipos del repositorio se gestionan como flakes.
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
-{
   environment.systemPackages = with pkgs; [
     git
     curl
