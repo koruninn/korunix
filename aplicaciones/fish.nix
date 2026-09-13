5db389b66b4314b679cb69de1c32bc2426c8d450
koruninn <koruninn@gmail.com>
2026-09-12 21:36:47 -0500

    Configurar Fish con Fastfetch

--- aplicaciones/fish.nix
+++ aplicaciones/fish.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   # Habilita Fish y lo deja como shell de inicio por defecto para los usuarios
   # normales que no definan otro shell explícitamente.
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set -g fish_greeting ""
+      fastfetch
+    '';
+  };
+
   users.defaultUserShell = pkgs.fish;
 }
