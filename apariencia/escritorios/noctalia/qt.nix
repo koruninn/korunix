7fab7a9fd02dbadaefca9c608f7ec88c7ce8df00
koruninn <koruninn@gmail.com>
2026-09-12 21:18:24 -0500

    Usar integración KDE para Qt en Noctalia

--- apariencia/escritorios/noctalia/qt.nix
+++ apariencia/escritorios/noctalia/qt.nix
@@ -1,38 +1,12 @@
-{
-  config,
-  pkgs,
-  ...
-}: let
-  qt6ctConfig = pkgs.writeText "korunix-qt6ct.conf" ''
-    [Appearance]
-    color_scheme_path=${config.users.users.koru.home}/.config/qt6ct/colors/noctalia.conf
-    custom_palette=true
-    icon_theme=Hatter-Slate
-    standard_dialogs=default
-    style=Fusion
-  '';
-in {
-  # Qt necesita exponer sus plugins en el perfil para que las aplicaciones
-  # lanzadas desde Niri puedan cargar qt6ct.
+{pkgs, ...}: {
+  # Las aplicaciones Qt/KDE de la sesión Niri usan la integración nativa de KDE.
+  # Noctalia escribe su KColorScheme en kdeglobals; esta integración hace que
+  # Dolphin, Kate, Kdenlive y otras aplicaciones Qt lean esos colores directamente.
   qt.enable = true;
 
-  environment.systemPackages = [
-    pkgs.qt6Packages.qt6ct
+  environment.systemPackages = with pkgs.kdePackages; [
+    kio
+    plasma-integration
+    systemsettings
   ];
-
-  # Dejamos qt6ct apuntando directamente a la paleta que genera Noctalia.
-  # La variable que activa qt6ct se define solo dentro de la sesión de Niri,
-  # por lo que Plasma mantiene su propia configuración.
-  system.activationScripts.noctaliaQtConfig.text = ''
-    install -d -m 0755 \
-      -o ${config.users.users.koru.name} \
-      -g ${config.users.users.koru.group} \
-      ${config.users.users.koru.home}/.config/qt6ct
-
-    install -m 0644 \
-      -o ${config.users.users.koru.name} \
-      -g ${config.users.users.koru.group} \
-      ${qt6ctConfig} \
-      ${config.users.users.koru.home}/.config/qt6ct/qt6ct.conf
-  '';
 }
