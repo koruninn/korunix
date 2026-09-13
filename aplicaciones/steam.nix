bbff3fdbf72fe52e153937d697c65ba2e5ca4154
koruninn <koruninn@gmail.com>
2026-09-12 20:07:40 -0500

    Integrar Millennium en Steam

--- aplicaciones/steam.nix
+++ aplicaciones/steam.nix
@@ -1,17 +1,19 @@
 {
-  config,
-  pkgs,
   inputs,
-  lib,
+  pkgs,
   ...
 }: {
+  # Millennium publica un overlay que sustituye Steam por su paquete integrado.
+  nixpkgs.overlays = [
+    inputs.millennium.overlays.default
+  ];
+
   programs.steam = {
     enable = true;
+    package = pkgs.millennium-steam;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-
   };
 
   programs.gamemode.enable = true;
-
 }
