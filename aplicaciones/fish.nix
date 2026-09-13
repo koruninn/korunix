643a33809b6e8729b84751a6549283c10ae015fb
koruninn <koruninn@gmail.com>
2026-09-12 21:33:10 -0500

    Usar Fish como shell por defecto

--- aplicaciones/fish.nix
+++ aplicaciones/fish.nix
@@ -1,6 +1,6 @@
-{ ... }: {
-  # Habilita el shell Fish y sus integraciones nativas en NixOS.
-  programs.fish = {
-    enable = true;
-  };
+{pkgs, ...}: {
+  # Habilita Fish y lo deja como shell de inicio por defecto para los usuarios
+  # normales que no definan otro shell explícitamente.
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
 }
