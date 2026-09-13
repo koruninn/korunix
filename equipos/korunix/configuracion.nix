1cc3da57ce10d8618efc0c76fc3715d65c26f24a
koruninn <koruninn@gmail.com>
2026-09-12 22:46:46 -0500

    Eliminar enlace heredado de Fish en Korunix

--- equipos/korunix/configuracion.nix
+++ equipos/korunix/configuracion.nix
@@ -1,26 +1,27 @@
-{ pkgs, ... }:
+{
+  equipo,
+  pkgs,
+  ...
+}:
 
 {
   time.timeZone = "America/Lima";
   i18n.defaultLocale = "es_PE.UTF-8";
   services.printing.enable = true;
 
-  users.users."koru" = {
+  users.users.${equipo.persona} = {
     isNormalUser = true;
     description = "André";
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
-  # Ajustes exclusivos de este equipo: monitor y dotfiles de koru.
+  # Ajustes exclusivos de este equipo: monitor.
   environment.etc."niri/monitor.kdl".text = ''
     output "DP-1" {
         mode "1920x1080@120.000"
         scale 1
     }
   '';
-  systemd.tmpfiles.rules = [
-    "L+ /home/koru/.config/fish/config.fish - - - - /home/koru/.korunix/config.fish"
-  ];
 
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = [ pkgs.alejandra ];
