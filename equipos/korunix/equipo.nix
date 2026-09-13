262a9a1eab1e6b1bdd485dd5dbe9c6077482e715
koruninn <koruninn@gmail.com>
2026-09-12 23:00:42 -0500

    Declarar pantalla principal de Korunix

--- equipos/korunix/equipo.nix
+++ equipos/korunix/equipo.nix
@@ -5,4 +5,12 @@
   # Persona principal del equipo. Los módulos reutilizables usan este nombre
   # en vez de asumir un usuario concreto.
   persona = "koru";
+
+  # Pantalla principal de este equipo. Los compositores pueden reutilizar estos
+  # datos sin incrustar detalles de hardware dentro de sus módulos.
+  pantalla = {
+    nombre = "DP-1";
+    modo = "1920x1080@120";
+    escala = 1.0;
+  };
 }
