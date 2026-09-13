34fd04cf6a7d54a1e3c0caff594ac7d2ecd6f8ce
koruninn <koruninn@gmail.com>
2026-09-13 01:56:51 -0500

    Adaptar audio de 32 bits a la arquitectura

--- modulos/base/audio.nix
+++ modulos/base/audio.nix
@@ -1,11 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
+    # Steam y otros programas x86 pueden necesitar bibliotecas de audio de
+    # 32 bits. En otras arquitecturas no intentamos habilitar una compatibilidad
+    # que no corresponde a la plataforma.
+    alsa.support32Bit = pkgs.stdenv.hostPlatform.isx86_64;
     pulse.enable = true;
   };
 }
