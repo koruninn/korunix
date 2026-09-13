df5744e1ce6c28dc2a763f3683744a3ab2c85a12
koruninn <koruninn@gmail.com>
2026-09-12 22:51:57 -0500

    Activar Noctalia Greeter

--- apariencia/greeter/default.nix
+++ apariencia/greeter/default.nix
@@ -1,9 +1,24 @@
 {
-  config,
   pkgs,
   ...
 }: {
-  imports = [    
-    
-  ];
+  # Korunix usa Noctalia Greeter sobre greetd como pantalla de inicio de sesión.
+  # El módulo oficial de NixOS habilita también Polkit y AccountsService.
+  services.displayManager.noctalia-greeter = {
+    enable = true;
+
+    settings = {
+      keyboard = {
+        layout = "es";
+        variant = "deadtilde";
+      };
+
+      cursor.size = 24;
+    };
+
+    cursorTheme = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+    };
+  };
 }
