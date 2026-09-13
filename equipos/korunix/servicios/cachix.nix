da2e5acaf2d1dd2322a6614005410dab752b6e53
koruninn <koruninn@gmail.com>
2026-09-13 01:26:02 -0500

    Evitar ajustes de Nix duplicados en Cachix

--- equipos/korunix/servicios/cachix.nix
+++ equipos/korunix/servicios/cachix.nix
@@ -1,18 +1,12 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{...}: {
   nix.settings = {
     extra-substituters = [
       "https://noctalia.cachix.org"
-      "https://ezkea.cachix.org" # <--- Añadido
+      "https://ezkea.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "ezkea.cachix.org-1:io85OCXmr5WwSZQYw7066RA2fNdOeOwGEgMDwiDxUCg=" # <--- Añadido
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtsWpOxNFQsBRglJzxWPp3dkU4="
+      "ezkea.cachix.org-1:io85OCXmr5WwSZQYw7066RA2fNdOeOwGEgMDwiDxUCg="
     ];
-
-    experimental-features = ["nix-command" "flakes"];
   };
 }
