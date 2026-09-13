87a7740ac4d8d9a7b5e08c6068d1c40d441dca82
koruninn <koruninn@gmail.com>
2026-09-12 22:46:17 -0500

    Declarar persona principal de Korunix

--- equipos/korunix/equipo.nix
+++ equipos/korunix/equipo.nix
@@ -1,4 +1,8 @@
 {
   canal = "unstable";
   arquitectura = "x86_64-linux";
+
+  # Persona principal del equipo. Los módulos reutilizables usan este nombre
+  # en vez de asumir un usuario concreto.
+  persona = "koru";
 }
