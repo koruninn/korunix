4cacaed33616a35c0076dcad0f05ced870aa9471
koruninn <koruninn@gmail.com>
2026-09-12 22:48:01 -0500

    Eliminar configuración antigua de Fish

--- config.fish
+++
@@ -1,8 +0,0 @@
-# Desactivar el saludo inicial por defecto de Fish
-set -g fish_greeting ""
-
-# Ejecutar fetch solo si la sesión es interactiva (la terminal está abierta).
-# Forzamos el mismo logo pequeño de NixOS que usaba Fastfetch.
-if status is-interactive
-    fetch -l nixos_small
-end
