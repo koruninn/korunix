a5f453f403d72b5373e1456f88b582f9ce4adbc4
koruninn <koruninn@gmail.com>
2026-09-15 16:58:49 -0500

    Formatear ajustes compartidos

--- apariencia/escritorios/compartido.nix
+++ apariencia/escritorios/compartido.nix
@@ -3,20 +3,65 @@
 
   # Una sola lista alimenta tanto el dock de Noctalia como los favoritos de GNOME.
   dock = [
-    {noctalia = "firefox"; gnome = "firefox.desktop";}
-    {noctalia = "org.gnome.Nautilus"; gnome = "org.gnome.Nautilus.desktop";}
-    {noctalia = "spotify"; gnome = "spotify.desktop";}
-    {noctalia = "steam"; gnome = "steam.desktop";}
-    {noctalia = "net.lutris.Lutris"; gnome = "net.lutris.Lutris.desktop";}
-    {noctalia = "anime-game-launcher"; gnome = "anime-game-launcher.desktop";}
-    {noctalia = "honkers-railway-launcher"; gnome = "honkers-railway-launcher.desktop";}
-    {noctalia = "vesktop"; gnome = "vesktop.desktop";}
-    {noctalia = "LocalSend"; gnome = "org.localsend.localsend_app.desktop";}
-    {noctalia = "code"; gnome = "code.desktop";}
-    {noctalia = "com.obsproject.Studio"; gnome = "com.obsproject.Studio.desktop";}
-    {noctalia = "org.kde.kdenlive"; gnome = "org.kde.kdenlive.desktop";}
-    {noctalia = "com.heroicgameslauncher.hgl"; gnome = "com.heroicgameslauncher.hgl.desktop";}
-    {noctalia = "onlyoffice-desktopeditors"; gnome = "onlyoffice-desktopeditors.desktop";}
-    {noctalia = "birdfont"; gnome = "birdfont.desktop";}
+    {
+      noctalia = "firefox";
+      gnome = "firefox.desktop";
+    }
+    {
+      noctalia = "org.gnome.Nautilus";
+      gnome = "org.gnome.Nautilus.desktop";
+    }
+    {
+      noctalia = "spotify";
+      gnome = "spotify.desktop";
+    }
+    {
+      noctalia = "steam";
+      gnome = "steam.desktop";
+    }
+    {
+      noctalia = "net.lutris.Lutris";
+      gnome = "net.lutris.Lutris.desktop";
+    }
+    {
+      noctalia = "anime-game-launcher";
+      gnome = "anime-game-launcher.desktop";
+    }
+    {
+      noctalia = "honkers-railway-launcher";
+      gnome = "honkers-railway-launcher.desktop";
+    }
+    {
+      noctalia = "vesktop";
+      gnome = "vesktop.desktop";
+    }
+    {
+      noctalia = "LocalSend";
+      gnome = "org.localsend.localsend_app.desktop";
+    }
+    {
+      noctalia = "code";
+      gnome = "code.desktop";
+    }
+    {
+      noctalia = "com.obsproject.Studio";
+      gnome = "com.obsproject.Studio.desktop";
+    }
+    {
+      noctalia = "org.kde.kdenlive";
+      gnome = "org.kde.kdenlive.desktop";
+    }
+    {
+      noctalia = "com.heroicgameslauncher.hgl";
+      gnome = "com.heroicgameslauncher.hgl.desktop";
+    }
+    {
+      noctalia = "onlyoffice-desktopeditors";
+      gnome = "onlyoffice-desktopeditors.desktop";
+    }
+    {
+      noctalia = "birdfont";
+      gnome = "birdfont.desktop";
+    }
   ];
 }
