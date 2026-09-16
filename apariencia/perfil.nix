09290b09cca0672d39f739a7074c2528943db1ac
koruninn <koruninn@gmail.com>
2026-09-15 19:26:34 -0500

    Mantener mutable el perfil de AccountsService

--- apariencia/perfil.nix
+++ apariencia/perfil.nix
@@ -10,14 +10,24 @@
     Icon=/var/lib/AccountsService/icons/${equipo.persona}
     SystemAccount=false
   '';
+
+  instalarPerfil = pkgs.writeShellApplication {
+    name = "korunix-profile-picture";
+    runtimeInputs = [pkgs.coreutils];
+    text = ''
+      install -d -m 0755 /var/lib/AccountsService/icons
+      install -d -m 0700 /var/lib/AccountsService/users
+      install -m 0644 ${equipo.foto} /var/lib/AccountsService/icons/${equipo.persona}
+      install -m 0600 ${cuenta} /var/lib/AccountsService/users/${equipo.persona}
+    '';
+  };
 in {
   # La foto pertenece a la persona del equipo, no a GNOME ni a Noctalia.
-  # AccountsService permite que ambos escritorios y el greeter lean la misma.
-  systemd.tmpfiles.rules = lib.optionals tieneFoto [
-    "d /var/lib/AccountsService 0755 root root -"
-    "d /var/lib/AccountsService/icons 0755 root root -"
-    "d /var/lib/AccountsService/users 0700 root root -"
-    "L+ /var/lib/AccountsService/icons/${equipo.persona} - - - - ${equipo.foto}"
-    "L+ /var/lib/AccountsService/users/${equipo.persona} - - - - ${cuenta}"
-  ];
+  # Se copia a AccountsService para que GNOME y el greeter compartan la misma
+  # sin convertir sus archivos mutables en enlaces al almacén de Nix.
+  system.activationScripts.korunixProfile = lib.mkIf tieneFoto {
+    text = ''
+      ${instalarPerfil}/bin/korunix-profile-picture
+    '';
+  };
 }
